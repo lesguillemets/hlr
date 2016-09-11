abc47fbe92c6db460cdf46774c1267e1eb681657
lesguillemets <lesguillemets@users.noreply.github.com>
2016-09-11 18:16:33 +0900

    save credentials

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Secret.Consumer (getKeys)
+import Secret.Token (getTokens)
 import Flickr.Auth
 import Flickr.Auth.Initial
 
@@ -9,4 +10,8 @@ main = do
     keys <- getKeys
     case keys of
          Nothing -> putStrLn "No consumer key/secret provided"
-         (Just k) -> initialise k >>= print
+         (Just k) -> do
+             creds <- getTokens
+             case creds of
+                  Nothing -> initialise k >>= print
+                  (Just c) -> print c
--- src/Secret/Token.hs
+++ src/Secret/Token.hs
@@ -1,8 +1,23 @@
 module Secret.Token
     (
+        getTokens
     ) where
 
-import qualified Data.ByteString.Char8 as BC
+import Prelude hiding (lookup)
+import Data.ByteString.Char8 (pack)
+import Data.Map (lookup)
 import Web.Authenticate.OAuth
 
 import Secret.Internal
+
+getTokens :: IO (Maybe Credential)
+getTokens = do
+    m <- readFileKVMaybe =<< getKeyfile
+    case m of
+         Nothing -> return Nothing
+         (Just d) ->
+            let lookFor k = pack <$> (k `lookup` d)
+                in
+                return $ newCredential
+                            <$> lookFor "token"
+                            <*> lookFor "tokenSecret"
