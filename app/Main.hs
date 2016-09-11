831b2a7e1aa4275defd54ec42bc27889bb26616f
lesguillemets <lesguillemets@users.noreply.github.com>
2016-09-11 18:37:21 +0900

    tidy up

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,7 @@
 module Main where
 
-import Secret.Consumer (getKeys)
-import Secret.Token (getTokens)
 import Flickr.Auth
-import Flickr.Auth.Initial
 
 main :: IO ()
-main = do
-    keys <- getKeys
-    case keys of
-         Nothing -> putStrLn "No consumer key/secret provided"
-         (Just k) -> do
-             creds <- getTokens
-             case creds of
-                  Nothing -> initialise k >>= print
-                  (Just c) -> print c
+main = start >>= print
+
--- src/Flickr/Auth.hs
+++ src/Flickr/Auth.hs
@@ -1,4 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Flickr.Auth where
+module Flickr.Auth
+    (
+        start
+    )
+    where
+
+
+import Web.Authenticate.OAuth
 
 import Flickr.Auth.Initial
+import Flickr.Auth.Common
+import Secret.Consumer (getKeys, KeyPair)
+import Secret.Token (getTokens)
+
+start :: IO (Maybe (OAuth, Credential))
+start = do
+    keys <- getKeys
+    case keys of
+         Nothing -> do
+             putStrLn "No consumer key/secret provided"
+             return Nothing
+         (Just k) -> do
+             cr <- getCreds k
+             return $ Just (oauth k, cr)
+
+getCreds :: KeyPair -> IO Credential
+getCreds k = do
+    creds <- getTokens
+    case creds of
+         Nothing -> initialise k
+         (Just c) -> return c
